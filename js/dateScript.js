2b093a937cb81e26723c35ca91f77a6935689002
M <mrgmckenzie@gmail.com>
2023-09-17 14:24:10 +0100

    styling of title

--- js/dateScript.js
+++ js/dateScript.js
@@ -10,5 +10,5 @@ const title = document.getElementById("title")
 dateElement.innerHTML = todayString
 
 if (today.getDay() === 0) {
-    title.innerHTML = "The Sunday mr-mckenzie.github.io Chronicle"
+    title.innerHTML = "The Sunday <span id='io'>mr-mckenzie.github.io</span> Chronicle"
 }
