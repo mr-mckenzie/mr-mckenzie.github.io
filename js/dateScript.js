42f82ab29930788fcbc9dc42a894697cca7de4fd
M <mrgmckenzie@gmail.com>
2023-09-17 11:54:35 +0100

    styling, date script now changes title on Sundays

--- js/dateScript.js
+++ js/dateScript.js
@@ -5,5 +5,10 @@ const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric
 const todayString = today.toLocaleDateString('en-GB', options)
 
 const dateElement = document.getElementById("date");
+const title = document.getElementById("title")
 
 dateElement.innerHTML = todayString
+
+if (today.getDay() === 0) {
+    title.innerHTML = "The Sunday mr-mckenzie.github.io Chronicle"
+}
